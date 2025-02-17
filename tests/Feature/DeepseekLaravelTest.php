fea03637f1bf089c5563d7a64511e140b4fbaf9c
dayCod <alayarahman07@gmail.com>
2025-02-17 14:14:14 +0700

    adds: laravel wrapper for deepseek

---
+++ tests/Feature/DeepseekLaravelTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Feature;
+
+use DeepSeekClient;
+use DeepSeek\Enums\Configs\DefaultConfigs;
+use Illuminate\Support\Facades\Config;
+use Orchestra\Testbench\TestCase;
+
+final class DeepseekLaravelTest extends TestCase
+{
+    //
+}
